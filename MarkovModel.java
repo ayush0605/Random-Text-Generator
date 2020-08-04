00790db64bfc6f7c0dbad3c497843171bb4af3e4
Ayush Singhal <40767716+ayush0605@users.noreply.github.com>
2020-08-04 21:58:07 +0530

    Add files via upload
    MarkovModel java class. This class contains getRandomText() method which generates text according to markov model of order n ( specified by the user).

---
+++ MarkovModel.java
@@ -0,0 +1,38 @@
+
+/**
+ * MarkovModel
+ * 
+ * @author (Ayush Singhal) 
+ * @version (1.0)
+ */
+import java.util.*;
+public class MarkovModel extends AbstractMarkovModel {
+
+    private int myOrder;
+    
+    public MarkovModel(int order){
+        myRandom = new Random();
+        myOrder = order;
+    }
+    
+    public String getRandomText(int numChars){
+        if(myText == null){
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        int index = myRandom.nextInt(myText.length()-myOrder);
+        String key = myText.substring(index,index+myOrder);
+        sb.append(key);
+        for(int i=0; i<numChars-myOrder ;i++){
+            ArrayList<String> follows = getFollows(key);
+            if(follows.size() == 0){
+                break;
+            }
+            index = myRandom.nextInt(follows.size());
+            String next = follows.get(index);
+            sb.append(next);
+            key = key.substring(1) + next; 
+        }
+        return sb.toString();
+    }
+}
