bbb42ecccd5cf13724390e33864d038893b91fc3
Ayush Singhal <40767716+ayush0605@users.noreply.github.com>
2020-08-04 19:14:08 +0530

    Add files via upload
    Abstract java class with implementation of getFollows method so that it can be used in each class extending this Abstract Class.

---
+++ AbstractMarkovModel.java
@@ -0,0 +1,45 @@
+
+/**
+ * Abstract class AbstractMarkovModel
+ * 
+ * @author (Ayush Singhal)
+ * @version (1.0)
+ */
+
+import java.util.*;
+
+public abstract class AbstractMarkovModel implements IMarkovModel {
+    protected String myText;
+    protected Random myRandom;
+    
+    public AbstractMarkovModel() {
+        myRandom = new Random();
+    }
+    
+    public void setTraining(String s) {
+        myText = s.trim();
+    }
+    
+    //Method to generate Random Text
+    // It is a abstract method. Must be defined in all classes extending this class
+    abstract public String getRandomText(int numChars);
+    
+    //Method to find the follow character
+    protected ArrayList<String> getFollows(String key){
+        ArrayList<String> follows = new ArrayList<String>(); 
+        int pos=0;
+        while(pos<myText.length()){
+            int start = myText.indexOf(key,pos);
+            if(start==-1){
+                break;
+            }
+            if(start+key.length() >= myText.length()){
+                break;
+            }
+            String next = myText.substring(start+key.length(),start+key.length()+1);
+            follows.add(next);
+            pos = start + key.length();
+        }
+        return follows;
+    }
+}
