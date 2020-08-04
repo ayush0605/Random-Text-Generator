618733df1a1224caa9d3d035b0a7a09d406d93d3
Ayush Singhal <40767716+ayush0605@users.noreply.github.com>
2020-08-04 18:58:56 +0530

    Add files via upload
    interface for the Markov Model

---
+++ IMarkovModel.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of interface IMarkovModel here.
+ * 
+ * @author (Ayush Singhal) 
+ * @version (1.0)
+ */
+
+public interface IMarkovModel {
+    public void setTraining(String text);
+    public void setRandom(int seed);
+    public String getRandomText(int numChars);
+    
+}
